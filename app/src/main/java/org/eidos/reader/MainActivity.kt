394b2453369d44c73d00ba1b2c93270d73b7c4d6
Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2021-01-01 11:02:05 +0800

    Add basic navigation drawer to application

--- app/src/main/java/org/eidos/reader/MainActivity.kt
+++ app/src/main/java/org/eidos/reader/MainActivity.kt
@@ -2,10 +2,57 @@ package org.eidos.reader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import org.eidos.reader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // finding the toolbar
+        val toolbar = binding.toolbar
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        drawerLayout = binding.drawerLayout
+
+        /*
+        This code sets up the tool bar with the hamburger menus and shit
+
+        Because the toolbar only appears in one place (aka the activity), the toolbar
+        will be similar throughout the entire application.
+
+        If we want to implement different toolbars for different fragments, we need to
+        set up the tool bar from within each fragment. That way, the fragment can control
+        the visuals of the app bar.
+
+        non-functional code for reference:
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        See below for more information about implementing different tool bars:
+        https://developer.android.com/guide/navigation/navigation-ui#support_app_bar_variations
+
+        For implementing the drawer
+        https://stackoverflow.com/questions/26440879/how-do-i-use-drawerlayout-to-display-over-the-actionbar-toolbar-and-under-the-st
+        https://developer.android.com/guide/navigation/navigation-ui#add_a_navigation_drawer
+         */
+        NavigationUI.setupWithNavController(toolbar, navController, drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
---
+++ app/src/main/java/org/eidos/reader/ui/fandoms/FandomTypeSelectionViewModel.kt
@@ -0,0 +1,7 @@
+package org.eidos.reader.ui.fandoms
+
+import androidx.lifecycle.ViewModel
+
+class FandomTypeSelectionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
