6b77e0b77a81312f378083a21bccac7779c93869
Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2021-06-14 13:12:05 +0800

    Add sort options

---
+++ app/src/main/java/org/eidos/reader/ui/misc/values/SortOptions.kt
@@ -0,0 +1,13 @@
+package org.eidos.reader.ui.misc.values
+
+val SORT_OPTIONS = mapOf(
+    "Author" to "authors_to_sort_on",
+    "Title" to "title_to_sort_on",
+    "Date Posted" to "created_at",
+    "Date Updated" to "revised_at",
+    "Word Count" to "word_count",
+    "Hits" to "hits",
+    "Kudos" to "kudos_count",
+    "Comments" to "comments_count",
+    "Bookmarks" to "bookmarks_count"
+)
